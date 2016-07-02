db75883ab0f1cd3b3e52632a6a4594be342628be
Dhinesh Ram <k.dhineshram@hotmail.com>
2016-07-02 12:24:25 +0530

    Check the number is positive negative or zero

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0)
+                Console.WriteLine("Positive");
+            else if (num < 0)
+                Console.WriteLine("Negative");
+            else
+                Console.WriteLine("Zero");
+            Console.ReadLine();
+        }
+    }
+}
