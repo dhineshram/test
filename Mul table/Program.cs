7259e2c218d2fd3917ce18c7cb79e0c6d1dc973c
Dhinesh Ram <k.dhineshram@hotmail.com>
2016-07-16 00:12:20 +0530

    aa

---
+++ Mul table/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i + " x " + n + " = " + i * n);
+            Console.ReadLine();
+        }
+    }
+}
