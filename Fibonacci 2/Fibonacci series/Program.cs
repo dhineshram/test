11cc60519b45f6f09cd74d9d916f6236b7c13cc8
Dhinesh Ram <k.dhineshram@hotmail.com>
2016-07-15 23:45:27 +0530

    repair duplicate

---
+++ Fibonacci 2/Fibonacci series/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonacci_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0, b = 1, i = 0;
+            Console.WriteLine("0\n1");
+            for (; i < n;)
+            {
+                i = a + b; 
+                a = b;
+                b = i;
+                if (i<n)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
